ff86febbc72b62e184c82f96f1ef5e471383b4a4
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-02-26 15:04:20 +1100

    Adds backend validation for schedule form and back button for error page

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ app.get('/', redirectLogin, (req, res) => {
 //get schedule management page
 //TO_CHAR() function converts a timestamp, an interval, an integer, a double precision, or a numeric value to a string
 //TO_CHAR(expression, format)
-app.get('/schedule', (req, res) => {
+app.get('/schedule', redirectLogin, (req, res) => {
     db.any(`SELECT day, TO_CHAR(start_time,'HH24:MI') start_time, TO_CHAR(end_time,'HH24:MI') end_time FROM schedules WHERE id_user = $1;`, [req.session.userId])
         .then((schedules) => {
             res.render('pages/schedule', {
@@ -249,19 +249,41 @@ app.get('/schedule', (req, res) => {
 //post schedule
 //the format we need HH24 and MI
 //TO_TIMESTAMP converts char of CHAR, VARCHAR2, NCHAR, or NVARCHAR2 datatype to a value of TIMESTAMP datatype.
-app.post('/schedule', (req, res) => {
+app.post('/schedule', redirectLogin, (req, res) => {
+    let dayValid = daysOfWeek.slice(1).includes(daysOfWeek[req.body.day])
+    console.log(daysOfWeek.slice(1))
+    let startValid = /^(([0-1][0-9])|(2[0-3])):[0-5][0-9]$/.test(req.body.start_time)
+    let endValid = /^(([0-1][0-9])|(2[0-3])):[0-5][0-9]$/.test(req.body.end_time)
 
-    db.query(`INSERT INTO schedules (id_user, day, start_time, end_time) 
-    VALUES ($1, $2, TO_TIMESTAMP($3,'HH24:MI'), TO_TIMESTAMP($4,'HH24:MI'))`, [req.session.userId, req.body.day, req.body.start_time, req.body.end_time])
-        .then((schedules) => {
-            res.redirect('schedule')
-        })
-        .catch((err) => {
+    if (dayValid && startValid && endValid) {
+        if (Number(req.body.end_time.replace(':', '')) > Number(req.body.start_time.replace(':', ''))) {
+            db.query(`INSERT INTO schedules (id_user, day, start_time, end_time) 
+            VALUES ($1, $2, TO_TIMESTAMP($3,'HH24:MI'), TO_TIMESTAMP($4,'HH24:MI'))`, [req.session.userId, req.body.day, req.body.start_time, req.body.end_time])
+                .then((schedules) => {
+                    return res.redirect('schedule')
+                })
+                .catch((err) => {
+                    return res.render('pages/error', {
+                        layout: './layouts/profile-layout',
+                        err: err
+                    })
+                })
+        } else {
             res.render('pages/error', {
                 layout: './layouts/profile-layout',
-                err: err
+                err: {
+                    message: 'End time must be later than the start time.'
+                }
             })
+        }
+    } else {
+        res.render('pages/error', {
+            layout: './layouts/profile-layout',
+            err: {
+                message: 'Inputs were not valid, please try again.'
+            }
         })
+    }
 })
 
 
